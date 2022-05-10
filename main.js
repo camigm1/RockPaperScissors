e4dab125736a16619141a7b176ca01ff9935672c
Camila Gallegos <camigm1@gmail.com>
2022-05-10 16:23:35 -0600

    updates

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const computerPlay = function () {
   const value = Math.random() * 10;
   if (value < 3) {
     return "rock";
-  } else if (3 > value) {
+  } else if (3 < value) {
     return "paper";
   } else if (value > 6) {
     return "scissors";
@@ -12,20 +12,25 @@ const computerPlay = function () {
 // console.log(computerPlay());
 const computerSelection = computerPlay();
 const playerSelection = "rock";
+let humanPlayerWon = `Player One wins!!!`;
+let computerWon = "Computer won this round!!";
 
 const playRound = function (playerSelection, computerSelection) {
+  let playerOne = playerSelection;
   if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
+    (playerOne === "rock" && computerSelection === "scissors") ||
+    (playerOne === "scissors" && computerSelection === "paper") ||
+    (playerOne === "paper" && computerSelection === "rock")
   ) {
-    return "Human Player wins!!!";
+    let humanPlayerWon = `Player One wins!!!`;
+    return humanPlayerWon;
   } else if (
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    return "Computer won this round!!";
+    let computerWon = "Computer won this round!!";
+    return computerWon;
   } else if (playerSelection === computerSelection) {
     return "Its a draw!!";
   } else {
@@ -33,4 +38,19 @@ const playRound = function (playerSelection, computerSelection) {
   }
 };
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+const game = function (playerSelection, computerSelection) {
+  let player = 0;
+  let computer = 0;
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+  }
+  if (humanPlayerWon) {
+    player += 1;
+  } else if (computerWon) {
+    computer = +1;
+  }
+};
+
+console.log(game());
