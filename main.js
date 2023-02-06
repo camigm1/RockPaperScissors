a25832735adad85c78155188c49a0cf6882f73fc
Camila Gallegos <camigm1@gmail.com>
2023-02-06 16:35:28 -0700

    updates

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+const results = document.querySelector(".result");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
 
 let valueRock = "";
 let valuePaper = "";
@@ -8,17 +11,17 @@ let valueScissors = "";
 
 rock.addEventListener("click", function () {
   valueRock = rock.innerHTML.toLowerCase();
-  console.log(playRound(valueRock, computerPlay()));
+  playRound(valueRock, computerPlay());
 });
 
 paper.addEventListener("click", function () {
   valuePaper = paper.innerHTML.toLowerCase();
-  console.log(playRound(valuePaper, computerPlay()));
+  playRound(valuePaper, computerPlay());
 });
 
 scissors.addEventListener("click", function () {
   valueScissors = scissors.innerHTML.toLowerCase();
-  console.log(playRound(valueScissors, computerPlay()));
+  playRound(valueScissors, computerPlay());
 });
 
 const computerPlay = function () {
@@ -31,36 +34,37 @@ const computerPlay = function () {
     return "scissors";
   }
 };
-
+// console.log(computerPlay());
 let humanPlayerWon = `Player One wins this round!`;
 let computerWon = "Computer won this round!";
-
+let playerOnePoints = 0;
+let computerSelectionPoints = 0;
 const playRound = function (playerSelection, computerSelection) {
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    return humanPlayerWon;
+    results.textContent = humanPlayerWon;
+    playerScore.textContent = playerOnePoints += 1;
   } else if (
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper")
   ) {
-    return computerWon;
-  } else if (playerSelection === computerSelection) {
-    return "Its a draw!!";
+    results.textContent = computerWon;
+    computerScore.textContent = computerSelectionPoints += 1;
   } else {
-    return "Please enter a value";
+    results.textContent = "Its a Draw";
   }
 };
 
-// let game = () => {
+// let game = (value) => {
 //   let playerOnePoints = 0;
 //   let computerSelectionPoints = 0;
 //   for (let i = 0; i < 5; i++) {
 //     let computerSelection = computerPlay();
-//     const result = playRound(computerSelection);
+//     const result = playRound(value, computerSelection);
 //     console.log(result);
 //     if (result === humanPlayerWon) {
 //       console.log((playerOnePoints += 1));
@@ -78,3 +82,22 @@ const playRound = function (playerSelection, computerSelection) {
 // };
 
 // game();
+
+// let game = (value) => {
+//   // console.log(result);
+//   let playerOnePoints = 0;
+//   let computerSelectionPoints = 0;
+//   if (value === humanPlayerWon) {
+//     console.log((playerOnePoints += 1));
+//   } else if (value === computerWon) {
+//     console.log((computerSelectionPoints += 1));
+//   }
+
+// if (playerOnePoints > computerSelectionPoints) {
+//   console.log("Player is Ultimate winner!!");
+// } else if (computerSelectionPoints > playerOnePoints) {
+//   console.log("Computer is ultimate winner");
+// } else {
+//   console.log("nobody is the winner");
+// }
+// };
